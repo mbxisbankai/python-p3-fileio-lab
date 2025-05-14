3533d17b87dd344bc3e0a120dc2f45373d0738e3
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-05-14 09:38:46 +0300

    File I/0 Lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
+    path = f"{file_name}.txt"
+    with open(path, mode="w", encoding="utf-8") as text_file:
+        text_file.write(file_content)
 
 def append_file(file_name, append_content):
+    path = f"{file_name}.txt"
+    with open(path, mode="a") as text_file:
+        text_file.write(append_content)
     pass
 
 def read_file(file_name):
-    pass
+    path = f"{file_name}.txt"
+    with open(path, mode="r", encoding="utf-8") as text_file:
+        return text_file.read()
+
